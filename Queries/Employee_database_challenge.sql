a884cb6b54f338d6fff1354bf3b80f97f80a7d00
Teresa Hartman <teresa2h@users.campus>
2022-08-24 22:34:41 -0500

    Including final query and csv files

--- Queries/Employee_database_challenge.sql
+++ Queries/Employee_database_challenge.sql
@@ -1,8 +1,6 @@
 --Employee_Database_challenge.sql
 
---DROP TABLE retirement_titles
-
--- Use Dictinct with Orderby to remove duplicate rows
+-- Create table including employees that may retire soon 
 SELECT --DISTINCT ON (e.emp_no)
 e.emp_no,
 e.first_name,
@@ -18,9 +16,6 @@ WHERE (e.birth_date BETWEEN '1952.01.01' AND '1955-12-31')
 --      AND (t.to_date = '9999-01-01')
 ORDER BY e.emp_no;
 
---SELECT * FROM retirement_titles
---DROP TABLE unique_titles
-
 --Use Dictinct with Orderby to remove duplicate rows
 SELECT DISTINCT ON (rt.emp_no)
 rt.emp_no,
@@ -31,7 +26,6 @@ INTO unique_titles
 FROM retirement_titles as rt
 WHERE (rt.to_date = '9999-01-01')
 
-
 --Number of employees about to retire by job title
 SELECT COUNT(ut.emp_no), ut.title
 INTO retiring_titles
@@ -40,7 +34,24 @@ GROUP BY ut.title
 ORDER BY COUNT(ut.emp_no) DESC;
 
 
-
+-- Create Mentoship Eligibility table
+SELECT DISTINCT ON (e.emp_no)
+e.emp_no,
+e.first_name,
+e.last_name,
+e.birth_date,
+de.from_date,
+de.to_date,
+t.title
+INTO mentorship_eligibility
+FROM employees as e 
+	INNER JOIN dept_emp as de
+		ON (e.emp_no = de.emp_no)
+	INNER JOIN titles as t
+		ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1965.01.01' AND '1965-12-31') 
+     AND (t.to_date = '9999-01-01')
+ORDER BY e.emp_no;
 
 
 
